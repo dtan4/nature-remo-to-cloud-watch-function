69ca606a4aa92671f4585c0fd1466d7569c7e8a3
Daisuke Fujita <dtanshi45@gmail.com>
2019-01-06 22:22:01 +0900

    Define a wrapper of Nature Remo API client

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/dtan4/nature-remo-to-cloud-watch-function
+
+require github.com/tenntenn/natureremo v0.0.1
---
+++ natureremo/natureremo.go
@@ -0,0 +1,10 @@
+package natureremo
+
+import (
+	"github.com/tenntenn/natureremo"
+)
+
+// Client represents a wrapper of Nature Remo API client
+type Client struct {
+	api *natureremo.Client
+}
