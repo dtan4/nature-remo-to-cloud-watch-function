d8563c36dfd23f4943c6b541f6b5f0bf196559c7
Daisuke Fujita <dtanshi45@gmail.com>
2019-01-08 01:32:40 +0900

    Retrieve secrets from Parameter Store

--- aws/ssm/ssm.go
+++ aws/ssm/ssm.go
@@ -1,7 +1,12 @@
 package ssm
 
 import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/aws/aws-sdk-go/service/ssm/ssmiface"
+	"github.com/pkg/errors"
 )
 
 // ClientInterface is an interface of a wrapper of SSM API client
@@ -19,4 +24,28 @@ func NewClient(api ssmiface.SSMAPI) *Client {
 	return &Client{
 		api: api,
 	}
+}
+
+// LoadSecrets retrieves decrypted secrets from Parameter Store
+func (c *Client) LoadSecrets(path string) (map[string]string, error) {
+	resp, err := c.api.GetParametersByPath(&ssm.GetParametersByPathInput{
+		Path:           aws.String(path),
+		Recursive:      aws.Bool(false),
+		WithDecryption: aws.Bool(true),
+	})
+	if err != nil {
+		return map[string]string{}, errors.Wrap(err, "cannot retrieve secrets from Parameter Store")
+	}
+
+	secrets := map[string]string{}
+
+	for _, param := range resp.Parameters {
+		name := aws.StringValue(param.Name)
+		name = strings.TrimPrefix(name, path)
+		name = strings.TrimPrefix(name, "/")
+
+		secrets[name] = aws.StringValue(param.Value)
+	}
+
+	return secrets, nil
 }
--- aws/ssm/ssm_test.go
+++ aws/ssm/ssm_test.go
@@ -1,10 +1,14 @@
 package ssm
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/dtan4/nature-remo-to-cloud-watch-function/aws/mock"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/golang/mock/gomock"
 )
 
@@ -19,5 +23,106 @@ func TestNewClient(t *testing.T) {
 	got := NewClient(ssmMock)
 	if got == nil {
 		t.Error("want object, got nil")
+	}
+}
+
+func TestLoadSecrets(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testcases := []struct {
+		path string
+		want map[string]string
+	}{
+		{
+			path: "/foobarbaz",
+			want: map[string]string{
+				"foo": "abc",
+				"bar": "def",
+			},
+		},
+		{
+			path: "/foobarbaz/",
+			want: map[string]string{
+				"foo": "abc",
+				"bar": "def",
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.path, func(t *testing.T) {
+			ssmMock := mock.NewMockSSMAPI(ctrl)
+			ssmMock.EXPECT().GetParametersByPath(&ssm.GetParametersByPathInput{
+				Path:           aws.String(tc.path),
+				Recursive:      aws.Bool(false),
+				WithDecryption: aws.Bool(true),
+			}).Return(&ssm.GetParametersByPathOutput{
+				Parameters: []*ssm.Parameter{
+					{
+						Name:  aws.String("/foobarbaz/foo"),
+						Value: aws.String("abc"),
+					},
+					{
+						Name:  aws.String("/foobarbaz/bar"),
+						Value: aws.String("def"),
+					},
+				},
+			}, nil)
+
+			client := &Client{
+				api: ssmMock,
+			}
+
+			got, err := client.LoadSecrets(tc.path)
+			if err != nil {
+				t.Fatalf("want no error, got: %s", err)
+			}
+
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("want: %q, got: %q", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestLoadSecrets_error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testcases := []struct {
+		subtitle string
+		path     string
+		want     string
+	}{
+		{
+			subtitle: "api error",
+			path:     "/foobarbaz",
+			want:     "cannot retrieve secrets from Parameter Store: unexpected error",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.subtitle, func(t *testing.T) {
+			ssmMock := mock.NewMockSSMAPI(ctrl)
+			ssmMock.EXPECT().GetParametersByPath(&ssm.GetParametersByPathInput{
+				Path:           aws.String(tc.path),
+				Recursive:      aws.Bool(false),
+				WithDecryption: aws.Bool(true),
+			}).Return(nil, fmt.Errorf("unexpected error"))
+
+			client := &Client{
+				api: ssmMock,
+			}
+
+			_, err := client.LoadSecrets(tc.path)
+			if err == nil {
+				t.Fatal("want error, got nil")
+			}
+
+			if err.Error() != tc.want {
+				t.Errorf("want: %q, got: %q", tc.want, err.Error())
+			}
+		})
 	}
 }
