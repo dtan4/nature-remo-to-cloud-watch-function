3f3e8a72818d422a92c371e38afa1a51e2db39f5
Daisuke Fujita <dtanshi45@gmail.com>
2020-08-30 16:38:34 +0900

    Introduce Sentry

--- function/main.go
+++ function/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/dtan4/nature-remo-to-cloud-watch-function/aws/cloudwatch"
@@ -14,19 +16,29 @@ import (
 	cloudwatchapi "github.com/aws/aws-sdk-go/service/cloudwatch"
 	ssmapi "github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/getsentry/sentry-go"
 	"github.com/pkg/errors"
 )
 
 const (
 	natureRemoAccessTokenKey = "/natureRemoToCloudWatchFunction/natureRemoAccessToken"
 	deviceIDKey              = "/natureRemoToCloudWatchFunction/deviceID"
+	sentryDsnKey             = "/natureRemoToCloudWatchFunction/sentryDsn"
 )
 
 var (
 	CloudWatchClient cloudwatch.ClientInterface
 	SSMClient        ssm.ClientInterface
 	NatureRemoClient natureremo.ClientInterface
+
+	sentryEnabled = false
 )
+
+func init() {
+	if strings.ToLower(os.Getenv("ENABLE_SENTRY")) == "true" {
+		sentryEnabled = true
+	}
+}
 
 func RealHandler(ctx context.Context) error {
 	deviceID, err := SSMClient.LoadSecret(ctx, deviceIDKey)
@@ -64,7 +76,33 @@ func Handler(ctx context.Context) error {
 
 	NatureRemoClient = natureremo.NewClient(accessToken, xray.Client(http.DefaultClient))
 
-	return RealHandler(ctx)
+	if sentryEnabled {
+		sentryDsn, err := SSMClient.LoadSecret(ctx, sentryDsnKey)
+		if err != nil {
+			return errors.Wrap(err, "cannot load Sentry DSN")
+		}
+
+		if err := sentry.Init(sentry.ClientOptions{
+			Dsn: sentryDsn,
+			Transport: &sentry.HTTPSyncTransport{
+				Timeout: 5 * time.Second,
+			},
+
+			// Release: version.Version,
+			// https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime
+			ServerName: os.Getenv("AWS_LAMBDA_FUNCTION_NAME"),
+		}); err != nil {
+			return errors.Wrap(err, "cannot initialize Sentry client")
+		}
+	}
+
+	if err := RealHandler(ctx); err != nil {
+		if sentryEnabled {
+			sentry.CaptureException(err)
+		}
+	}
+
+	return nil
 }
 
 func main() {
